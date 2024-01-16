2c2d305cc70846742ad11ed7ebe9bb846a552d59
IlliaRohalskyi <2004lion2004@gmail.com>
2024-01-17 00:36:16 +0100

    Data ingestion added

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,98 @@
+"""
+Data Ingestion Module.
+
+This module provides a class with a method for data ingestion tasks
+"""
+import os
+
+import pandas as pd
+import psycopg2
+
+from src.logger import logging
+from src.utility import get_cfg, get_root
+class DataIngestion:
+    """
+    Data ingestion class.
+
+    This class provides a method for data ingestion tasks.
+
+    Attributes:
+        ingestion_config (DataIngestionConfig): The configuration settings for data ingestion tasks.
+    """
+
+    def __init__(self):
+        """
+        Initialize the DataIngestion instance.
+
+        Args:
+            ingestion_config (dict): Configuration settings for data ingestion tasks.
+        """
+        self.ingestion_config = get_cfg(os.path.join(get_root(), ".cfg/components/data_ingestion.yaml"))
+
+    def initiate_data_ingestion(self) -> pd.DataFrame:
+        """
+        Initiate the data ingestion process.
+
+        This method triggers the ingestion of data as specified
+        in the configuration. It returns the pandas dataframe
+
+        Returns:
+            pandas.DataFrame: A DataFrame containing the ingested training data
+        """
+
+        logging.info("Initiating data ingestion")
+
+        file_type = self.ingestion_config['training_data_path'].split(".")[-1]
+
+        file_path = os.path.join(get_root(), self.ingestion_config['training_data_path'])
+
+        if file_type == "xls" or file_type == "xlsx":
+            data = pd.read_excel(file_path)
+        elif file_type == "csv":
+            data = pd.read_csv(file_path)
+        else:
+            logging.error("Unsupported data type error")
+            raise UnsupportedFileTypeError(file_type)
+
+        logging.info("Data ingestion completed successfully")
+        return data
+
+    def get_sql_table(self, table_name):
+        """
+        Initiate the data ingestion process from a Postgres database.
+
+        This method triggers the ingestion of data from the database
+
+        Returns:
+            pandas.DataFrame: A DataFrame containing the ingested data.
+        """
+        hostname = os.environ.get("DB_HOSTNAME")
+        database_name = os.environ.get("DB_NAME")
+        username = os.environ.get("DB_USERNAME")
+        password = os.environ.get("DB_PASSWORD")
+
+        try:
+            connection = psycopg2.connect(
+                host=hostname, database=database_name, user=username, password=password
+            )
+
+        except psycopg2.ConnectionError as e:
+            logging.error("Error connecting to PostgreSQL:", e)
+
+        query = f"SELECT * FROM {table_name};"
+
+        try:
+            pd.read_sql_query(query, connection)
+        except Exception as e:
+            logging.error("Error executing SQL query:", e)
+
+        finally:
+            if connection:
+                connection.close()
+
+class UnsupportedFileTypeError(Exception):
+    def __init__(self, file_type):
+        super().__init__(f"The file type '{file_type}' is not supported. Supported file types are: .xlsx, .xls, and .csv.")
+
+if __name__ == '__main__':
+    print(DataIngestion().initiate_data_ingestion())
--- src/utility.py
+++ src/utility.py
@@ -9,7 +9,7 @@ Example:
     print(f"The logger path is: {config_file['path']}")
 """
 import yaml
-
+import os
 
 def get_cfg(path, encoding="utf-8"):
     """
@@ -25,3 +25,21 @@ def get_cfg(path, encoding="utf-8"):
 
     with open(path, encoding=encoding) as file:
         return yaml.safe_load(file)
+
+def get_root() -> str:
+    """
+    Get Project Root Directory.
+
+    This function determines the root directory of a project based on the location of the script.
+    It navigates upwards in the directory tree until it finds the setup.py file.
+
+    Returns:
+        str: The absolute path of the project's root directory.
+    """
+    script_path = os.path.abspath(__file__)
+
+    # Navigate upwards in the directory tree until you find the setup.py file
+    while not os.path.exists(os.path.join(script_path, "setup.py")):
+        script_path = os.path.dirname(script_path)
+
+    return script_path
