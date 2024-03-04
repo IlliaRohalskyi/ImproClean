4ca0050be0c07968dec4e5110f2561b71a6a1218
IlliaRohalskyi <2004lion2004@gmail.com>
2024-03-04 22:34:57 +0100

    Retraining Pipeline Added

--- src/pipelines/retraining_pipeline.py
+++ src/pipelines/retraining_pipeline.py
@@ -1,36 +1,39 @@
 """
-Module for defining a Prefect flow to orchestrate a machine learning training pipeline.
+Module for defining a Prefect flow to orchestrate a machine learning retraining pipeline.
 
 This module includes tasks for data ingestion, transformation, validation,
-train-test split, and model training.
+train-test split, and model retraining.
 
 It uses components from the src.components package to streamline the workflow.
 """
+
 from prefect import flow
 
 from src.pipelines.common.training_tasks import (
     get_data,
+    retrain_model,
     select_data,
-    train_model,
     train_test_split,
     validate_data,
 )
+from src.utility import get_cfg
 
 
-@flow(name="train_pipeline")
-def training_pipeline():
+@flow(name="retrain_pipeline")
+def retraining_pipeline():
     """
-    Prefect flow to orchestrate the entire training pipeline.
+    Prefect flow to orchestrate the entire retraining pipeline.
 
     Returns:
         None
     """
+    cfg = get_cfg("pipelines/retraining_pipeline.yaml")
     data = get_data()
     selected_data = select_data(data)
     validated_data = validate_data(selected_data)
     train_test_split_data = train_test_split(validated_data)
-    train_model(train_test_split_data)
+    retrain_model(train_test_split_data, cfg)
 
 
 if __name__ == "__main__":
-    training_pipeline()
+    retraining_pipeline()
