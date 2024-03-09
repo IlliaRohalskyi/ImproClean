3ab5889f0729f2d2b6c3775677a7dc7864742a97
IlliaRohalskyi <2004lion2004@gmail.com>
2024-03-09 22:58:57 +0100

    Predictor class added

--- src/components/model_loader.py
+++ src/components/model_loader.py
@@ -2,15 +2,64 @@
 Model Loader Script
 
 This script provides functions to download machine learning models
-from the MLFlow model registry
+from the MLFlow model registry.
 """
 
-from typing import Tuple
+import ast
+from typing import List, Tuple
 
 import mlflow
 
 
-def get_models(model_names: Tuple[str] = ("xgb", "rf")):
+# pylint: disable=R0903
+class Predictor:
+    """
+    A class for making ensemble predictions using MLFlow models and weights.
+    """
+
+    def __init__(self, models, weights):
+        """
+        Initialize the Predictor.
+
+        Args:
+            models (dict): Dictionary of MLFlow models.
+            weights (dict): Dictionary of model weights.
+        """
+        if len(models) == 0 or len(weights) == 0:
+            raise ValueError("Models and weights must not be empty")
+
+        if len(models) != len(weights):
+            raise ValueError("Number of models must be equal to the number of weights")
+
+        self.models = models
+        self.weights = weights
+
+    def predict(self, input_data):
+        """
+        Make ensemble predictions.
+
+        Args:
+            input_data: Input data for prediction.
+
+        Returns:
+            float: Ensemble prediction.
+        """
+        if not all(model_name in self.models for model_name in self.weights):
+            raise ValueError(
+                "All models in weights must be present in the models dictionary"
+            )
+
+        ensemble_prediction = 0.0
+
+        for model_name, model in self.models.items():
+            weight = self.weights.get(model_name, 0.0)
+            model_prediction = model.predict(input_data)
+            ensemble_prediction += weight * model_prediction
+
+        return ensemble_prediction
+
+
+def get_models(model_names: Tuple[str] = ("xgb", "rf")) -> Predictor:
     """
     Load models from the MLFlow model registry.
 
@@ -18,9 +67,11 @@ def get_models(model_names: Tuple[str] = ("xgb", "rf")):
         model_names (Tuple[str]): Names of the MLFlow models to load.
 
     Returns:
-        List: A list of model objects which stores the loaded model
+        Predictor: An instance of the Predictor class.
     """
-    models = []
+    models = {}
+    weights = []
+
     for model_name in model_names:
         latest_versions = mlflow.tracking.MlflowClient().get_latest_versions(
             name=model_name
@@ -28,7 +79,6 @@ def get_models(model_names: Tuple[str] = ("xgb", "rf")):
 
         if latest_versions:
             latest_version = latest_versions[0]
-            print(latest_version)
             model = (
                 mlflow.xgboost.load_model(f"runs:/{latest_version.run_id}/{model_name}")
                 if model_name == "xgb"
@@ -36,5 +86,49 @@ def get_models(model_names: Tuple[str] = ("xgb", "rf")):
                     f"runs:/{latest_version.run_id}/{model_name}"
                 )
             )
-            models.append(model)
-    return model
+            models[model_name] = model
+
+            try:
+                weights_str = (
+                    mlflow.tracking.MlflowClient()
+                    .get_run(latest_version.run_id)
+                    .data.params["weights"]
+                )
+                weights_dict = ast.literal_eval(weights_str)
+                weights.append(weights_dict)
+
+            except:  # pylint: disable=W0702
+                pass
+
+    weight = check_weights(weights, model_names)
+
+    return Predictor(models, weight)
+
+
+def check_weights(weights: List[dict], model_names: Tuple[str]) -> dict:
+    """
+    Check and adjust weights.
+
+    Args:
+        weights (List[dict]): List of weight dictionaries.
+        model_names (Tuple[str]): Names of the MLFlow models.
+
+    Returns:
+        dict: Adjusted weights.
+    """
+    if len(weights) > 1 and all(
+        all(abs(weights[i][key] - weights[j][key]) < 1e-5 for key in weights[i])
+        for i in range(len(weights))
+        for j in range(i + 1, len(weights))
+    ):
+        weight = weights[0]
+    else:
+        num_models = len(model_names)
+        weight = {model: 1 / num_models for model in model_names}
+
+    return weight
+
+
+if __name__ == "__main__":
+    a = get_models()
+    print(a.models, a.weights)
