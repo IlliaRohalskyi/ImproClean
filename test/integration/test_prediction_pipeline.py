82ac469a1e9201c43b71815b74ca758ebd3b547c
IlliaRohalskyi <2004lion2004@gmail.com>
2024-03-20 14:58:01 +0100

    Small workflow adjustment

--- test/integration/test_prediction_pipeline.py
+++ test/integration/test_prediction_pipeline.py
@@ -1,13 +1,29 @@
+"""
+Module for integration testing of the prediction pipeline.
+
+This module contains an integration test function that uploads synthetic data,
+runs the prediction pipeline, and then deletes the specified table from the database.
+"""
+
 import os
 from test.test_utility import upload_data
 
-import pytest
+import psycopg2
 
 from src.pipelines.prediction_pipeline import prediction_pipeline
 from src.utility import get_cfg, get_root
 
 
 def test_pred_integration():
+    """
+    Integration test for the prediction pipeline.
+
+    This test uploads synthetic data, runs the prediction pipeline, and then deletes
+    the specified table from the database.
+
+    Returns:
+        None
+    """
     test_data_path = os.path.join(
         get_root(),
         "test",
@@ -18,3 +34,22 @@ def test_pred_integration():
     cfg = get_cfg("test/integration/test_prediction_pipeline.yaml")
     upload_data(test_data_path, cfg["prediction_table"])
     prediction_pipeline(cfg)
+
+    hostname = os.environ.get("DB_HOSTNAME")
+    database_name = os.environ.get("DB_NAME")
+    username = os.environ.get("DB_USERNAME")
+    password = os.environ.get("DB_PASSWORD")
+
+    connection = psycopg2.connect(
+        host=hostname, database=database_name, user=username, password=password
+    )
+
+    cursor = connection.cursor()
+
+    delete_query = f"DELETE FROM {cfg['write_table']}"
+
+    cursor.execute(delete_query)
+    connection.commit()
+
+    cursor.close()
+    connection.close()
