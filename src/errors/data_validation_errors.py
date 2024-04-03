d47c3f912970cfc182ca5a012e40cf19bcf65bf1
IlliaRohalskyi <2004lion2004@gmail.com>
2024-04-03 15:14:05 +0200

    Column diff error expanded

--- src/errors/data_validation_errors.py
+++ src/errors/data_validation_errors.py
@@ -5,11 +5,26 @@ This module contains all the errors for data_validation.py module
 
 class ColumnsDiffError(Exception):
     """
-    Error that is raised when columns are different between dataframes
+    Error that is raised when columns are different between DataFrames and provides details about the difference.
     """
 
-    def __init__(self):
-        super().__init__("Columns are different between dataframes")
+    def __init__(self, df1, df2):
+        """
+        Args:
+            df1: The first DataFrame to compare.
+            df2: The second DataFrame to compare.
+        """
+        self.df1_cols = df1.columns.tolist()
+        self.df2_cols = df2.columns.tolist()
+        self.diff_cols = set(self.df1_cols) ^ set(self.df2_cols)
+        message = "Columns are different between DataFrames:\n"
+        if self.diff_cols:
+            message += f"Missing columns in DataFrame1: {', '.join(self.diff_cols - set(self.df2_cols))}\n"
+            message += f"Missing columns in DataFrame2: {', '.join(self.diff_cols - set(self.df1_cols))}"
+        else:
+            if self.df1_cols != self.df2_cols:
+                message += "Column order is different between DataFrames."
+        super().__init__(message)
 
 
 class DtypeDiffError(Exception):
